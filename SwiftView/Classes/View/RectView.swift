6a2d28f16f79f9cd0142c39c71cdb0bd069fd671
DLK <1455801522@qq.com>
2026-01-22 14:38:20 +0800

    1、提交代码

---
+++ SwiftView/Classes/View/RectView.swift
@@ -0,0 +1,21 @@
+//
+//  RectView.swift
+//  SwiftView_Example
+//
+//  Created by MR_DLK on 2026/1/22.
+//  Copyright © 2026 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class RectView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
